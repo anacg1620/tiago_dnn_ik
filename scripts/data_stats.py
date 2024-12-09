aeba5d90e53b13856a6120194ccd14dc2a0d7487
anacg1620 <anacg1620@gmail.com>
2024-12-09 11:41:28 +0100

    modify data stats to add distance

--- scripts/data_stats.py
+++ scripts/data_stats.py
@@ -2,24 +2,13 @@
 
 import yaml
 import pandas as pd
+import numpy as np
 import os
 
 with open(os.path.join('/tiago_public_ws/src/tiago_data_generation/', 'config/datagen_controller.yaml')) as f:
     config = yaml.safe_load(f)
 
 df = pd.read_csv(os.path.join('/home/anacg/.ros', config['datagen_controller']['filename']))
+df['distance'] = np.sqrt(np.square(df['ee_x']) + np.square(df['ee_y']) + np.square(df['ee_z']))
 
-print('max x value: ')
-print(df['ee_x'].max())
-print('min x value: ')
-print(df['ee_x'].min())
-
-print('max y value: ')
-print(df['ee_y'].max())
-print('min y value: ')
-print(df['ee_y'].min())
-
-print('max z value: ')
-print(df['ee_z'].max())
-print('min z value: ')
-print(df['ee_z'].min())
+print(df[['ee_x', 'ee_y', 'ee_z', 'distance']].describe())
