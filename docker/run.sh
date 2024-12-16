996ea6517af7947eb12515e56c37422d885f73c8
anacg1620 <anacg1620@gmail.com>
2024-12-16 14:18:33 +0100

    add docker scripts

---
+++ docker/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+set -u
+
+if [ $# -eq 0 ]
+then
+    echo "running docker without display"
+    docker run -it --rm -w /home/tiago_dnn_ik --gpus all --name tiago_dnn_ik_container tiago_dnn_ik /bin/bash
+else
+    export DISPLAY=$DISPLAY
+	echo "setting display to $DISPLAY"
+	xhost +
+	docker run -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix -e NVIDIA_VISIBLE_DEVICES=all -w /home/tiago_dnn_ik --runtime=nvidia --gpus=all --name=tiago_dnn_ik_container tiago_dnn_ik /bin/bash
+	xhost -
+fi
