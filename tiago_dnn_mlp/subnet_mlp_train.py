0c66e44c498569f49720bff299e88e850c9df605
anacg1620 <anacg1620@gmail.com>
2025-03-26 12:21:42 +0100

    metrics for 7subnet

--- tiago_dnn_mlp/subnet_mlp_train.py
+++ tiago_dnn_mlp/subnet_mlp_train.py
@@ -7,6 +7,161 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
+from pykin.robots.single_arm import SingleArm
+from pykin.kinematics.transform import Transform
+
+
+current_curr = 1
+file_path = 'urdf/tiago/tiago.urdf'
+robot = SingleArm(file_path, Transform(rot=[0.0, 0.0, 0.0], pos=[0, 0, 0]))
+
+
+class UpdateLRCurriculumOnPlateau(tf.keras.callbacks.Callback):
+    def __init__(self, monitor="val_loss", factor=0.1, patience=10, verbose=0, mode="auto", 
+                 min_delta=1e-4, cooldown=0, min_lr=0.0, max_lr=0.1, total_currs=1, **kwargs):
+        super().__init__()
+
+        self.monitor = monitor
+        if factor >= 1.0:
+            raise ValueError(f"ReduceLROnPlateau does not support a factor >= 1.0. Received factor={factor}")
+
+        self.total_currs = total_currs
+        self.factor = factor
+        self.min_lr = min_lr
+        self.max_lr = max_lr
+        self.min_delta = min_delta
+        self.patience = patience
+        self.verbose = verbose
+        self.cooldown = cooldown
+        self.cooldown_counter = 0  # Cooldown counter.
+        self.wait = 0
+        self.best = 0
+        self.mode = mode
+        self.monitor_op = None
+        self._reset()
+
+    def _reset(self):
+        """Resets wait counter and cooldown counter."""
+        if self.mode not in {"auto", "min", "max"}:
+            print(f"Learning rate reduction mode {self.mode} is unknown, fallback to auto mode.", stacklevel=2,)
+            self.mode = "auto"
+
+        if self.mode == "min" or (
+            self.mode == "auto" and "acc" not in self.monitor
+        ):
+            self.monitor_op = lambda a, b: np.less(a, b - self.min_delta)
+            self.best = np.inf
+
+        else:
+            self.monitor_op = lambda a, b: np.greater(a, b + self.min_delta)
+            self.best = -np.inf
+
+        self.cooldown_counter = self.cooldown
+        self.wait = 0
+
+    def on_train_begin(self, logs=None):
+        self._reset()
+
+    def on_epoch_end(self, epoch, logs=None):
+        global current_curr
+
+        logs = logs or {}
+        logs["learning_rate"] = float(np.array(self.model.optimizer.learning_rate))
+        current = logs.get(self.monitor)
+
+        if current is None:
+            print(
+                "Learning rate reduction is conditioned on metric "
+                f"`{self.monitor}` which is not available. Available metrics "
+                f"are: {','.join(list(logs.keys()))}.",
+                stacklevel=2,
+            )
+        else:
+            # Update LR
+            if self.in_cooldown():
+                self.cooldown_counter -= 1
+                self.wait = 0
+            if self.monitor_op(current, self.best):
+                self.best = current
+                self.wait = 0
+            elif not self.in_cooldown():
+                self.wait += 1
+                if self.wait >= self.patience:
+                    old_lr = float(np.array(self.model.optimizer.learning_rate))
+                    
+                    if old_lr > np.float32(self.min_lr):
+                        new_lr = old_lr * self.factor
+                        new_lr = max(new_lr, self.min_lr)
+                        self.model.optimizer.learning_rate = new_lr
+
+                        if self.verbose > 0:
+                            print(f"\nEpoch {epoch + 1}:  Reducing learning rate to {new_lr}.")
+
+                        self.cooldown_counter = self.cooldown
+                        self.wait = 0
+
+                    else:
+                        if current_curr < self.total_currs:
+                            current_curr += 1
+                            print(f'\n\n--- Change to curriculum {current_curr} ---\n')
+                            self.model.optimizer.learning_rate = self.max_lr
+                            self._reset()
+                        elif self.model.optimizer.learning_rate == self.min_lr:
+                            print('All curriculums and LRs tested. Stopping...')
+                            self.model.stop_training = True
+
+    def in_cooldown(self):
+        return self.cooldown_counter > 0
+
+
+def position_error(stats):
+    def pos_error(y_true, y_pred):
+        # De-standardization or de-normalization of inputs
+        if 'std' == stats['norm']:
+            y_true = y_true * np.array(stats['df_std_out']) + np.array(stats['df_mean_out'])
+            y_pred = y_pred * np.array(stats['df_std_out']) + np.array(stats['df_mean_out'])
+        elif 'norm' == stats['norm']:
+            y_true = y_true * (np.array(stats['df_max_out']) - np.array(stats['df_min_out'])) + np.array(stats['df_min_out'])
+            y_pred = y_pred * (np.array(stats['df_max_out']) - np.array(stats['df_min_out'])) + np.array(stats['df_min_out'])
+        elif 'max-abs' == stats['norm']:
+            y_true = y_true * np.array(stats['df_maxabs_out'])
+            y_pred = y_pred * np.array(stats['df_maxabs_out'])
+        elif 'iqr' == stats['norm']:
+            y_true = y_true * (np.array(stats['df_quantile75_out']) - np.array(stats['df_quantile25_out'])) + np.array(stats['df_median_out'])
+            y_pred = y_pred * (np.array(stats['df_quantile75_out']) - np.array(stats['df_quantile25_out'])) + np.array(stats['df_median_out'])
+
+        fk_true = np.array([robot.forward_kin(y)['tiago_link_ee'].pos for y in y_true])
+        fk_pred = np.array([robot.forward_kin(y)['tiago_link_ee'].pos for y in y_pred])
+
+        return np.linalg.norm(fk_true - fk_pred, axis=1)
+    
+    return pos_error
+
+
+def orientation_error(stats):
+    def orient_error(y_true, y_pred):
+        # De-standardization or de-normalization of inputs
+        if 'std' == stats['norm']:
+            y_true = y_true * np.array(stats['df_std_out']) + np.array(stats['df_mean_out'])
+            y_pred = y_pred * np.array(stats['df_std_out']) + np.array(stats['df_mean_out'])
+        elif 'norm' == stats['norm']:
+            y_true = y_true * (np.array(stats['df_max_out']) - np.array(stats['df_min_out'])) + np.array(stats['df_min_out'])
+            y_pred = y_pred * (np.array(stats['df_max_out']) - np.array(stats['df_min_out'])) + np.array(stats['df_min_out'])
+        elif 'max-abs' == stats['norm']:
+            y_true = y_true * np.array(stats['df_maxabs_out'])
+            y_pred = y_pred * np.array(stats['df_maxabs_out'])
+        elif 'iqr' == stats['norm']:
+            y_true = y_true * (np.array(stats['df_quantile75_out']) - np.array(stats['df_quantile25_out'])) + np.array(stats['df_median_out'])
+            y_pred = y_pred * (np.array(stats['df_quantile75_out']) - np.array(stats['df_quantile25_out'])) + np.array(stats['df_median_out'])
+
+        fk_true = np.array([robot.forward_kin(y)['tiago_link_ee'].rot for y in y_true])
+        fk_pred = np.array([robot.forward_kin(y)['tiago_link_ee'].rot for y in y_pred])
+        dif1 = np.linalg.norm(fk_true - fk_pred, axis=1)
+        dif2 = np.linalg.norm(fk_true + fk_pred, axis=1)
+
+        return np.minimum(dif1, dif2)
+    
+    return orient_error
 
 
 class SevenSubnetMlp():
@@ -58,7 +213,10 @@ class SevenSubnetMlp():
         layer7 = layers.Dense(self.config['units7'], activation="relu", name="layer7") (input7)
         output7 = layers.Dense(1, activation = "linear", name="q7")(layer7)
 
-        self.model = keras.Model(inputs=[inputX, inputY, inputZ], outputs=[output1, output2, output3, output4, output5, output6, output7], name="ConcatMLP")
+        # combined outputs
+        combined_outputs = layers.concatenate([output1, output2, output3, output4, output5, output6, output7], name="combined_output")
+
+        self.model = keras.Model(inputs=[inputX, inputY, inputZ], outputs=[output1, output2, output3, output4, output5, output6, output7, combined_outputs], name="ConcatMLP")
         
         self.model.summary()
         # keras.utils.plot_model(self.model, 'tiago_dnn_mlp/concat_mlp_model.png', show_shapes=True)
@@ -89,6 +247,7 @@ if __name__ == '__main__':
     q5_ = y_train[:,[4]]
     q6_ = y_train[:,[5]]
     q7_ = y_train[:,[6]]
+    combined_ = y_train
 
     x_t = x_test[:,[0]]
     y_t = x_test[:,[1]]
@@ -101,25 +260,64 @@ if __name__ == '__main__':
     q5_t = y_test[:,[4]]
     q6_t = y_test[:,[5]]
     q7_t = y_test[:,[6]]
+    combined_t = y_test
 
     with open(f"data/{dnn.config['data_dir']}/data_stats.yaml") as f:
         stats = yaml.safe_load(f)
 
-    wandb.init(project='tiago_dnn_ik_tests', name="SevenSubnetMlp", tensorboard=True, config=dnn.config)
+    wandb.init(project='tiago_dnn_ik_results', name="SevenSubnetMlp", tensorboard=True, config=dnn.config)
 
     # Specify the loss fuction, optimizer, metrics
     dnn.model.compile(
         loss = 'mean_squared_error',
         optimizer = tf.keras.optimizers.Adam(learning_rate=dnn.config['lr']),
-        metrics = ['mean_squared_error', 'accuracy'],
+        metrics = ['mean_squared_error', 'mean_squared_error', 'mean_squared_error', 'mean_squared_error', 'mean_squared_error', 'mean_squared_error', 'mean_squared_error',
+                    ['mean_squared_error', 'accuracy', position_error(stats), orientation_error(stats)]],
         run_eagerly=True # to access individual elements in loss funct 
     )
 
     callbacks_list = [
         keras.callbacks.TensorBoard(log_dir="logs/"),
-        keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, verbose=dnn.config['verbose'], 
-                                    min_lr=0.0001, max_lr=dnn.config['lr'], cooldown=5)
+        UpdateLRCurriculumOnPlateau(monitor='val_loss', factor=0.5, patience=5, verbose=dnn.config['verbose'], 
+                                    min_lr=0.0001, max_lr=dnn.config['lr'], total_currs=stats['curriculums'], 
+                                    cooldown=5),
     ]
 
-    history = dnn.model.fit({'Xcoor':x, 'Ycoor':y, 'Zcoor':z}, {'q1': q1_, 'q2':q2_, 'q3':q3_, 'q4':q4_, 'q5':q5_, 'q6':q6_, 'q7':q7_}, 
-                            epochs=200, validation_split=0.3, batch_size=dnn.config['batch_size'], callbacks=callbacks_list, verbose=2)
+    # Train
+    history = dnn.model.fit({'Xcoor':x, 'Ycoor':y, 'Zcoor':z}, 
+                            {'q1': q1_, 'q2':q2_, 'q3':q3_, 'q4':q4_, 'q5':q5_, 'q6':q6_, 'q7':q7_, 'combined_output': combined_}, 
+                            epochs=200, 
+                            validation_data=({'Xcoor':x_t, 'Ycoor':y_t, 'Zcoor':z_t}, 
+                                             {'q1': q1_t, 'q2':q2_t, 'q3':q3_t, 'q4':q4_t, 'q5':q5_t, 'q6':q6_t, 'q7':q7_t, 'combined_output': combined_t}), 
+                            batch_size=dnn.config['batch_size'], 
+                            callbacks=callbacks_list, 
+                            verbose=dnn.config['verbose'])
+    
+    # Validation
+    x_val = np.load(f"data/{dnn.config['data_dir']}/x_val.npy")
+    y_val = np.load(f"data/{dnn.config['data_dir']}/y_val.npy")
+
+    x_v = x_val[:,[0]]
+    y_v = x_val[:,[1]]
+    z_v = x_val[:,[2]]
+
+    q1_v = y_val[:,[0]]
+    q2_v = y_val[:,[1]]
+    q3_v = y_val[:,[2]]
+    q4_v = y_val[:,[3]]
+    q5_v = y_val[:,[4]]
+    q6_v = y_val[:,[5]]
+    q7_v = y_val[:,[6]]
+    combined_v = y_val
+    
+    eval = dnn.model.evaluate({'Xcoor':x_v, 'Ycoor':y_v, 'Zcoor':z_v}, 
+                              {'q1': q1_v, 'q2':q2_v, 'q3':q3_v, 'q4':q4_v, 'q5':q5_v, 'q6':q6_v, 'q7':q7_v, 'combined_output': combined_v}, 
+                              batch_size=dnn.config['batch_size'], 
+                              callbacks=callbacks_list)
+    
+    wandb.log({'evaluation': {
+                'mse': eval[0], 
+                'accuracy': eval[1], 
+                'pos_error': eval[3], 
+                'orient_error': eval[4]
+                }})
